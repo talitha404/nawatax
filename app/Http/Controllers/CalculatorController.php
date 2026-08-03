76e7c34964c813461b4626fdfea7757aff7e25ab
Talitha <talithacandra140206@gmail.com>
2026-08-03 17:14:42 +0700

    add calculator controller

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Request\CalculateRequest;
+use App\Services\ProfitCalculatorService;
+use Illuminate\View\View;
 
 class CalculatorController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('calculator.index');
+    }
+
+    public function calculate(
+        CalculateRequest $request,
+        ProfitCalculatorService $profitCalculatorService,
+        ): View {
+        $result = $profitCalculatorService->calculate($request->validated());
+
+        return view('calculator.index', compact('result'));
+    }
 }
